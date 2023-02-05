76eeacea33901ece28123b6008c7567a866b0fc7
levialmog <almoglevi1008@gmail.com>
2023-02-05 18:15:56 +0200

    Game graphics and the logic of the turns are completed

--- tic-tac-toe/src/Controller.js
+++ tic-tac-toe/src/Controller.js
@@ -7,17 +7,21 @@ import MyContext from './Context'
 class Controller extends Component {
   constructor(){
     super()
-    this.state={turn:true}
+    this.state={turn: true}
+  }
+
+  changeTurn = () => {
+      this.setState({turn: !this.state.turn})
   }
 
   render() {
     return (
-      <MyContext.Provider turn={this.state.turn}>
+      <MyContext.Provider value={{turn:this.state.turn, changeTurn: this.changeTurn}}>
         <div className="Controller" >
           <h1>Tic Tac Toe</h1>
           <h2>Welcome!</h2>
           <BoardComp size={3}/> <br/>
-          <h1 style={{ position: 'fixed', bottom: 20, right: 550 }}>
+          <h1 style={{position: 'fixed', bottom: 20, right: 550}}>
             Player turn: {this.state.turn ? "X" : "O"}
           </h1>
         </div>
